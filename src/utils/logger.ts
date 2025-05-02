1aa80e420820e381b53f5b3a09be0859eb07bcfc
donnykk <donnykk9527@gmail.com>
2025-05-02 15:39:59 +0800

    fix: log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -14,7 +14,7 @@ export const logger = winston.createLogger({
     format: logFormat,
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
+        new winston.transports.File({ filename: './log/error.log', level: 'error' }),
+        new winston.transports.File({ filename: './log/combined.log' })
     ]
 });
